eb39e5a5b9c521d113c397b0d8c2da8f0fb13c30
Jorgeeerdssd <150379015+Jorgeeerdssd@users.noreply.github.com>
2024-11-12 08:43:12 -0600

    a

---
+++ prueba_django/myproyect/todos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    proveedor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+# Create your models here.
